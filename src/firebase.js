2e11c862536e1df4b6e385a54d20b1eb914929fb
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2021-04-02 21:23:18 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,7 @@
 import firebase from 'firebase'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBHq90yJun_nP91bJvDVwkrsm0dR3UJi4A",
-  authDomain: "myntra-chatapp.firebaseapp.com",
-  projectId: "myntra-chatapp",
-  storageBucket: "myntra-chatapp.appspot.com",
-  messagingSenderId: "1072835173963",
-  appId: "1:1072835173963:web:c54dfc10f3c3c85a41f7ce",
-  measurementId: "G-4HW4SJTLZD"
-};
+
 
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
