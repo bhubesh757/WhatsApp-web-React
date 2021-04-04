e2c5f806bd3a515cfe1fae7b8635aad840f50f96
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2021-04-04 22:05:47 +0530

    Add files via upload

--- src/Whatsapp-clone/Chat.js
+++ src/Whatsapp-clone/Chat.js
@@ -1,6 +1,6 @@
-import { Avatar, IconButton } from '@material-ui/core';
+import { Avatar, Button, IconButton } from '@material-ui/core';
 import React , {useEffect , useState} from 'react'
-
+import PublishIcon from '@material-ui/icons/Publish';
 import {useParams} from 'react-router-dom'
 
 // icons
@@ -10,13 +10,16 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { InsertEmoticon, SearchOutlined } from '@material-ui/icons';
 import MicIcon from '@material-ui/icons/Mic';
 import './Chat.css'
-import { db } from '../firebase';
+import { db , storage } from '../firebase';
 import firebase from '../firebase'
 import { useStateValue } from '../StateProvider';
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 // Flipmove
 
 import FlipMove from 'react-flip-move';
-function Chat() {
+// import imageUpload from './imageUpload';
+function Chat({}) {
 
     const [seed, setseed] = useState('')
     const [input, setinput] = useState('')
@@ -26,7 +29,18 @@ function Chat() {
 
     const [messages, setmessages] = useState([])
 
+    // images
+    const [images, setimages] = useState([]);
+
     const [{user} , dispatch] = useStateValue();
+
+
+
+    const [image, setimage] = useState(null);
+    // const [text, settext] = useState('');
+    const [progress, setprogress] = useState(0)
+
+
 
     useEffect(() => {
         if (roomId) {
@@ -35,6 +49,7 @@ function Chat() {
                 setroomName (snapshot.data().name)
             ))
 
+            
             db.collection('rooms').doc(roomId).collection('messages').orderBy('timestamp' , 'asc')
             .onSnapshot(snapshot => (
                 setmessages (snapshot.docs.map(
@@ -42,9 +57,33 @@ function Chat() {
                 ))
             ))
 
-             
+
+            
         }
     }, [roomId])
+
+
+    // useeffect for images
+   
+    useEffect(() => {
+        let unsubscribe ;
+
+        if (roomId) {
+            unsubscribe = db.collection('rooms')
+            .doc(roomId)
+            .collection('images')
+            .orderBy('timestamp' , 'desc')
+            .onSnapshot((snapshot) => {
+                setimages(snapshot.docs.map((doc) => doc.data()))
+            })
+        }
+
+        return () => {
+            unsubscribe ();
+        };
+    
+    }, [roomId])
+
 
     useEffect(() => {
         setseed( Math.floor(Math.random() * 5000));
@@ -56,6 +95,7 @@ function Chat() {
         e.preventDefault();
 
         console.log('boom typed' , input);
+        
 
         db.collection('rooms').doc(roomId).collection('messages')
         .add({
@@ -68,7 +108,75 @@ function Chat() {
         setinput('');
     }
 
+    // AddImage
+
+
+    // Adding image using the onchange
+
+    const onChange = (e) => {
+
+    }
+
+    // showOptions
+
+    const showOptions = () => {
+        console.log('showed options')
+       
+    }
     
+
+    // handlechange
+
+    const handleChange = (e) => {
+        if (e.target.files[0]){
+            setimage(e.target.files[0])
+        }
+    };
+
+    const handleUpload = () => {
+        const uploadTask = storage.ref(`images/${image.name }`).put(image);
+
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                // progress function...
+
+                const progress = Math.round(
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                )
+
+                setprogress(progress);
+            },
+            (error) => {
+             console.log(error);
+             alert(error.message);
+            },
+            
+            () => {
+                // complete function
+                storage 
+                .ref('images')
+                .child(image.name)
+                .getDownloadURL()
+                .then(url => {
+                    // post image to db
+
+                    db.collection('rooms').doc(roomId).collection('images')
+                    .add({
+                        timestamp : firebase.firestore.FieldValue.serverTimestamp(),
+                        name : user.displayName,
+                        imageUrl : url,
+                        // username : username,
+                    });
+                    
+                    setprogress(0);
+                    // settext('');
+                    setimage(null);
+                    // setavatar(null);
+                })
+            }
+        )
+    }
     return (
         <div className = 'chat'>
             {/* <h1> Boom i am Chat</h1> */}
@@ -95,13 +203,16 @@ function Chat() {
 
             <div className="chat__body">
                 <FlipMove>
+                    {/* messages */}
 
                 {
+                    
                     messages.map((message) => (
-
+                        
                 <p className={`chat__message ${ message.name === user.displayName && 'chat__receiver'}`}>
                     <span className = 'chat__username'>
                         {message.name}
+                        
                     </span>
                     
                     {message.message}
@@ -113,12 +224,61 @@ function Chat() {
                 </p>
                     )) 
                 }
-                </FlipMove>
+                    
+                {/* Adding images here */}
+                
+                {
+                    images.map((image) => (
 
+                <p className={`chat__image ${image.name === user.displayName && 'chat__receiver_image'}`}>
+                    <span className = 'chat__username'>
+                        {image.name}
+                        {/* bhubesh */}
+                    </span>
+                    {/* need to add image */}
+                    <div>
+                        
+                <img className = 'post_image' src ={image.imageUrl}></img>
             </div>
+                    <span className = 'chat__timestamp'>
+                    {new Date (image.timestamp?.toDate()) .toUTCString()} 
+                    </span>
+
+                </p>
+                    )) 
+                }
+               
+                </FlipMove>
+            </div>
+          
+
+            {/* chat body for the photo */}
 
             <div className="chat__footer">
-            <InsertEmoticon></InsertEmoticon>
+            <IconButton> <AddCircleIcon onClick = {showOptions} color="disabled" fontSize="large" ></AddCircleIcon> 
+            </IconButton> 
+            <IconButton>
+                <div>
+
+                 <PhotoLibraryIcon  onClick={() =>
+                          document.getElementById("share_img").click()
+                        } color="disabled" fontSize="large" >
+                </PhotoLibraryIcon> 
+               
+
+                <input
+                        type="file"
+                        id="share_img"
+                        style={{ display: "none" }}
+                        onChange = {handleChange}
+                        // accept="image/"
+                      />
+                </div>
+            </IconButton>
+            <IconButton>
+
+                      <PublishIcon  onClick = {handleUpload}></PublishIcon>
+            </IconButton>
             <form>
                 <input value = {input} onChange = {e => setinput(e.target.value)}  type = 'text' placeholder = 'Type a message' >
                 </input>
